cc379d0f96d7c92430b013d2dfd3bffcb3f9927d
Arctiqa <Sinitro@yandex.ru>
2024-01-19 23:08:40 +0800

    add homework-4, postgreSQL

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,29 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
-
+CREATE TABLE student
+(
+	student_id serial,
+	first_name varchar,
+	last_name varchar,
+	birthday date,
+	phone varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+ALTER TABLE student ADD COLUMN middle_name varchar
 
 -- 3. Удалить колонку middle_name
-
+ALTER TABLE student DROP COLUMN middle_name
 
 -- 4. Переименовать колонку birthday в birth_date
-
+ALTER TABLE student DROP COLUMN middle_name
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+ALTER TABLE student ALTER COLUMN phone SET DATA TYPE varchar(32)
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
+INSERT INTO student (first_name, last_name, birth_date) VALUES ('Ivan', 'Ivanov', '26.08.1997');
+INSERT INTO student (first_name, last_name, birth_date) VALUES ('Petr', 'Petrov', '21.04.1989');
+INSERT INTO student (first_name, last_name, birth_date) VALUES ('Fyodor', 'Vedrov', '7.08.1994')
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+TRUNCATE TABLE student RESTART IDENTITY
